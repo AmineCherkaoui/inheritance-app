2deef79fbc76576b6aa22c56eaf7d0900245e54e
Amine Cherkaoui <cherkaoui_amine@outlook.com>
2026-08-18 12:17:33 +0100

    minor changes

--- src/pages/CalculatorPageV3.jsx
+++ src/pages/CalculatorPageV3.jsx
@@ -662,7 +662,7 @@ export default function CalculatorPageV3() {
                 onClick={handlePrev}
                 disabled={activeMiniStepIndex === 0 && !isViewingResults}
                 className={cn(
-                  'px-6 py-3 rounded-md ring-1 ring-primary-950 bg-transparent text-primary-950 font-bold text-xs sm:text-sm transition-all flex items-center gap-1.5',
+                  'px-6 py-3 rounded-md hover:bg-primary-950/10 ring-1 ring-primary-950 bg-transparent text-primary-950 font-bold text-xs sm:text-sm transition-all flex items-center gap-1.5',
                   activeMiniStepIndex === 0 && !isViewingResults
                     ? 'opacity-40 cursor-not-allowed'
                     : 'hover:bg-default-100 cursor-pointer'
